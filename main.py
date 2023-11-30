556aecd9003bf5726055512f317a4e29fe0322d8
Tanzeel Jaffery <tanzeeljaffery@Tanzeels-MacBook-Air.local>
2023-11-30 15:47:01 -0600

    updates

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ while game_is_on:
     snake.move()
     scoreboard.write_scoreboard()
 
-    #TODO: #4 Detect collision with food
+    # TODO: #4 Detect collision with food
+
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
@@ -47,13 +48,13 @@ while game_is_on:
         scoreboard.clear()
         scoreboard.write_scoreboard()
 
-    #TODO: #6 Detect collision with wall
+    # TODO: #6 Detect collision with wall
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
 
-    #TODO: #7 Detect collision with tail
+    # TODO: #7 Detect collision with tail
 
     # if head collides with any segment in the tail
 
