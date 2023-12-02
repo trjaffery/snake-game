560d3e41a15585b88700d34ec3ffdea6f4b25d62
Tanzeel Jaffery <trjaffery04@gmail.com>
2023-12-02 01:06:26 -0600

    updates

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ while game_is_on:
     #TODO: #6 Detect collision with wall
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #TODO: #7 Detect collision with tail
 
@@ -59,7 +59,8 @@ while game_is_on:
 
     for segment in snake.all_segments[1:]:
          if snake.head.distance(segment) < 10:
-             game_is_on = False
-             scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -6,13 +6,15 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt", mode='r') as high_score_file:
+            self.high_score = int(high_score_file.read())
 
     def write_scoreboard(self):
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(x=0, y=260)
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def clear(self):
         super().clear()
@@ -20,9 +22,18 @@ class Scoreboard(Turtle):
     def increase_score(self):
         self.score += 1
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.penup()
-        self.hideturtle()
-        self.color("white")
-        self.write("GAME OVER!", align = ALIGNMENT, font = FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode='w') as high_score_file:
+                high_score_file.write(str(self.high_score))
+        self.score = 0
+        self.clear()
+        self.write_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(x=0, y=0)
+    #     self.penup()
+    #     self.hideturtle()
+    #     self.color("white")
+    #     self.write("GAME OVER!", align = ALIGNMENT, font = FONT)
